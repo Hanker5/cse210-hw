60742a17bc7a6ba1015a41189b2d19324f447f74
Hanker5 <hankryhays@gmail.com>
2025-09-21 14:28:29 +1000

    completed W01 prove assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your favorite first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.Write("What is your favorite last name? ");
+        string last_name = Console.ReadLine();
+
+        Console.Write($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        if (grade >= 90)
+        {
+            // A
+            Console.WriteLine("Your grade is an A!");
+        }
+        else if (grade >= 80)
+        {
+            //B
+            Console.WriteLine("Your grade is a B!");
+        }
+        else if (grade >= 70)
+        {
+            //C
+            Console.WriteLine("Your grade is a C!");
+        }
+        else if (grade >= 60)
+        {
+            //D
+            Console.WriteLine("Your grade is a D!");
+        }
+        else
+        {
+            //F
+            Console.WriteLine("Your grade is an F!");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You failed... Better Luck next time!");
+        }
     }
 }
