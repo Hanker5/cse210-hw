fef13b2480f94491148446626e007bc3cf9c1d56
Hanker5 <hankryhays@gmail.com>
2025-11-01 10:49:18 -0600

    completed week 7 prepare

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        MathAssignment math1 = new MathAssignment("Johny Depp","Decimals","6.9","6 & 7");
+
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssignment write1 = new WritingAssignment("Billy","Bob","Geometry for Ocelots by Exurb1a");
+
+        Console.WriteLine(write1.GetWritingInformation());
     }
 }
